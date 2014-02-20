06b71d8c877cdeffac4b8e0e4797b1e96aed0f4b
Tom Miller <jackerran@gmail.com>
2014-02-20 08:41:39 -0600

    Hacked together linked list in C

--- linked_list.c
+++ linked_list.c
@@ -1,22 +1,71 @@
+#include <stdlib.h>
 #include <stdio.h>
-
-#define MAX 5
 
 struct node {
 	int value;
 	struct node *next;
 };
 
+#define NODE_SIZE sizeof(struct node)
+
+void apply(void (*fn)(struct node *), struct node *head);
+struct node *create_list(int num);
+void print(struct node *item);
+void free_list(struct node *item);
+
 int main()
 {
-	int a = MAX;
-	struct node *a, *b;
-	a->value = 1;
-	a->next = b;
+	struct node *list = create_list(1024);
 
-	printf("A: %d B: %d", 5, 3)
+	apply(&print, list);
 
-	free(a);
+	free_list(list);
 	return 0;
 }
 
+void apply(void (*fn)(struct node *), struct node *head)
+{
+	struct node *item = head;
+	while(item) {
+		fn(item);
+		item = item->next;
+	}
+}
+
+struct node *create_list(int num)
+{
+	if(num > 0) {
+		int i;
+		struct node *head, *curr, *next;
+		head = calloc(1, NODE_SIZE);
+		head->value = 0;
+		curr = head;
+		for(i = 1; i < num; i++) {
+			next = calloc(1, NODE_SIZE);
+			next->value = i;
+			curr->next = next;
+			curr = next;
+		}
+		return head;
+	} else {
+		return NULL;
+	}
+}
+
+void print(struct node *item)
+{
+	printf("%d\n", item->value);
+	if(item->next == NULL)
+		printf("No more nodes.\n");
+}
+
+void free_list(struct node *head)
+{
+	struct node *tmp;
+
+	while(head) {
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
